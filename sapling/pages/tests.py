3e856d843975bd5671ac4c01d8208b9943a17e10
Nuno Maltez <nuno@cognitiva.com>
2012-05-30 18:19:23 +0100

    Some fixes to tests. I still need to check against current main

--- sapling/pages/tests.py
+++ sapling/pages/tests.py
@@ -408,7 +408,7 @@ class MergeModelFormTest(TestCase):
         a_post['contents'] = 'a contents'
         a = TestForm(a_post, instance=m_new)
         self.failIf(a.is_valid())
-        self.failUnless(MergeMixin.conflict_error in str(a.errors))
+        self.failUnless(unicode(MergeMixin.conflict_error) in unicode(a.errors))
 
         #repeated save with the same form rendered again should work, though
         a_post = a.data
